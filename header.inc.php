8dcbf2743042d72f9529dd061b0757d60a15cae8
jim-sudo <jimmyosburn@rocketmail.com>
2025-06-03 20:47:23 -0700

    Basic filtering again

--- header.inc.php
+++ header.inc.php
@@ -9,8 +9,8 @@
 <!-- This is a common navigation base to call different web pages on the website -->
 <div class="topnav">
     <a href="index.php">Home</a>
-    <a href="list_customers.php">List Customers</a>
-    <a href="list_products.php">List Products</a>
+    <a href="list_readers.php">List Readers</a>
+    <a href="list_issues.php">List Issues</a>
 </div></div>
 <h1>Comic Book Database (Spooderman)</h1>
 
